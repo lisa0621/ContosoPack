f1eaf303ef8faffe431007c03fd97772d3690607
Will <doggy.huang@gmail.com>
2018-02-03 02:17:30 +0800

    Initial release: v1.0.0

---
+++ Contoso.Library/SayHello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contoso.Library {
+    public class SayHello {
+        public string Hello () {
+            return $"Hello World! 1.0.0";
+        }
+        public string Welcome () {
+            return $"Welcome to the .NET world!";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ContosoPack
+
+練習建立與發佈 NuGet 套件
